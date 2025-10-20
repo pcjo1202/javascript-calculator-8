3992f087dce061bedf84751b29ba3c35e858c0b7
박창조 <ckdwh1202@gmail.com>
2025-10-20 14:17:19 +0900

    feat: 문자열 파싱 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   #input;
+  #separator;
+  #numberArray;
 
   constructor() {
-    this.#input = "";
+    this.#input = "//;\\n1;2;3";
+    this.#separator = null;
+    this.#numberArray = [];
   }
 
   /**
+   * 문자열 입력 받기
    * @returns {Promise<string>}
    */
   async input() {
@@ -16,6 +21,41 @@ class App {
     );
   }
 
+  /**
+   * 문자열 파싱
+   * @returns {void}
+   */
+  parseInput() {
+    const input = this.#input;
+
+    if (this.isCustomSeparator(input)) {
+      const prefix = input.split("\\n")[0]; // "//;\\n1;2;3" -> "//;"
+      const str = input.split("\\n")[1]; // "//;\\n1;2;3" -> "1;2;3"
+
+      this.#separator = prefix.split("//")[1]; // "//;" -> ";"
+      this.#numberArray = str.split(this.#separator).map(Number);
+
+      return;
+    }
+
+    // 3. 기본 구분자(쉼표, 콜론) 사용 및 문자열 분리
+    this.#numberArray = input.split(/[,:]/g).map(Number);
+  }
+
+  /**
+   * 커스텀 구분자 체크
+   * @param {string} str
+   * @returns {boolean}
+   */
+  isCustomSeparator(str) {
+    return str.startsWith("//") && str.includes("\\n");
+  }
+
+  /**
+   * 출력 기능
+   * @param {number} result
+   * @returns {void}
+   */
   output(result) {
     MissionUtils.Console.print(`결과 : ${result}`);
   }
@@ -23,6 +63,8 @@ class App {
   async run() {
     // 1. 문자열 입력 받기
     this.#input = await this.input();
+    // 2. 문자열 파싱
+    this.parseInput();
   }
 }
 
