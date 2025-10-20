9be7e124b0b51c473ac86cbf52a93e602058965f
박창조 <ckdwh1202@gmail.com>
2025-10-20 11:08:23 +0900

    feat: 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  #input;
+
+  constructor() {
+    this.#input = "";
+  }
+
+  /**
+   * @returns {Promise<string>}
+   */
+  async input() {
+    return await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+  }
+
+  async run() {
+    // 1. 문자열 입력 받기
+    this.#input = await this.input();
+  }
 }
 
 export default App;
